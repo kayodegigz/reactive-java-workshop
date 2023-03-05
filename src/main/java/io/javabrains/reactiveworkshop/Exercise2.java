ec2f1dc31174987a761a6e06c5b60709ca46c1e9
kayodegigz <kayogunrinde@gmail.com>
2023-03-05 17:14:35 +0100

    pushing solutions for ex 1 to 4 for reactive

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -13,10 +13,18 @@ public class Exercise2 {
 
         // the subscribe method is basically saying, listen to this reactive stream.
         // for every time a value is passed into it, carry out the func inside it.
+        // the subscribe method can also take multiple parameters,
+        // the second param it takes is an error object in case of a failure event
+        // the third argument is what is called on a completion event
+        // Print the value from intNumberMono when it emits
         ReactiveSources.
                 intNumbersFlux().
-                subscribe(e -> System.out.println(e));
-        
+                subscribe(
+                        num -> System.out.println(num),
+                        err -> System.out.println(err.getMessage()),
+                        () -> System.out.println("Complete")
+                );
+
         // Print all users in the ReactiveSources.userFlux stream
         ReactiveSources.
                 userFlux().
--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,15 +1,26 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+        // Use ReactiveSources.intNumberFlux() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFlux().
+//                subscribe(
+//                        num -> System.out.println(num),
+//                        err -> System.out.println(err.getMessage()),
+//                        () -> System.out.println("Done!")
+//                );
+
+        // The BaseBubscriber class can be used like this, instead of the lambdas written above
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         // Subscribe to a flux using an implementation of BaseSubscriber
         // TODO: Write code here
@@ -18,4 +29,22 @@ public class Exercise5 {
         System.in.read();
     }
 
+}
+
+
+// what this class is for is to replace the lambdas being written inside the subscribe method.
+// this class can be used instead, it extends BaseSubscriber
+// the class has an knows what to do for every event that happens.
+class MySubscriber<T> extends BaseSubscriber<T> {
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribe happened");
+        request(1);
+    }
+
+    @Override
+    protected void hookOnNext(T value) {
+        System.out.println(value.toString() + " received");
+        request(1);
+    }
 }

fe9218dcc564963c23459f2395c9ffd4b5639a97
kayodegigz <kayogunrinde@gmail.com>
2023-03-10 20:58:34 +0100

    pushing solutions for exception handling

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,30 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        // in this case, count() returns a Mono of type long. The reason for this is so
+        // the method can be non blocking
+        // the Mono is returned when there's a terminal event
+        ReactiveSources.
+                intNumbersFlux().
+                count().
+                subscribe(System.out::println);
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        // collectList() method returns a Mono of type list.
+        // collecting to a list normally would be blocking
+        ReactiveSources.
+                intNumbersFlux().
+                collectList().
+                subscribe(System.out::println);
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        // what this does is it returns a list of the length specified
+        // in this case, a list of 2 items
+        ReactiveSources.
+                intNumbersFlux().
+                buffer(2).
+                map(list -> list.get(0) + list.get(1)).
+                subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
